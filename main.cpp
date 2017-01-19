62b72c3648a9e5ed8007eadd69a3941622ce57b6
nullbytes0x00 <nullbytes0x00@gmail.com>
2017-01-19 15:41:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,23 +2,23 @@
 #include <windows.h>
 
 using namespace std;
-typedef void (*SomeFunction)(const LPCSTR);
+typedef void (*SomeFunction)(const LPCSTR); //defining a type associated with the function
 
 HINSTANCE dll;
 
 int main()
 {
-    dll = LoadLibrary("defaultdll.dll");
+    dll = LoadLibrary("defaultdll.dll"); //loading the dynamic library
 
     if (dll != 0){
         cout << "The DLL is loaded.";
-        SomeFunction MsgBox = (SomeFunction)GetProcAddress(dll, "SomeFunction");
+        SomeFunction MsgBox = (SomeFunction)GetProcAddress(dll, "SomeFunction"); //calling the function which is contained in the DLL
         MsgBox((const LPCSTR)"The DLL was loaded into memory successfully.");
-        FreeLibrary((HMODULE)dll);
+        FreeLibrary((HMODULE)dll); //freeing the memory which was used by DLL
     }
     else
     {
-        cout << "Something went wrong.\n";
+        cout << "Something went wrong.\n"; //this will be shown if the DLL fails to load
     }
     return 0;
 }
